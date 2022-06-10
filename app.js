dcbd36f24460d0780cbf87827b7a68e169b40b5e
DESKTOP-S8ABVPR\82109 <hs183000@naver.com>
2022-06-11 01:07:16 +0900

    Context

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
  const canvas = document.getElementById("jsCanvas");
+ const ctx= canvas.getContext("2d")
+ 
+  ctx.strokeStyle ="#2c2c2c";
+  ctx.lineWidth=2.5;
  let painting =false;
 
   function stopPainting(){
    painting=false;
   }
+  function startPainting(){
+   painting=true;
+  }
  function onMousemove(event){
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+      ctx.beginPath()
+      ctx.moveTo(x,y)
+    }else{
+      ctx.lineTo(x,y)
+      ctx.stroke()
+    }
     
  }
  function onMousedown(event){
-    console.log(event);
+    
     painting=true;
  }
- function onMouseup (event){
-   stopPainting()
- }
+
  
  
  if(canvas){
     canvas.addEventListener("mousemove",onMousemove)
-    canvas.addEventListener("mousedown",onMousedown)
-    canvas.addEventListener("mouseup",onMouseup)
+    canvas.addEventListener("mousedown",startPainting)
+    canvas.addEventListener("mouseup",stopPainting)
     canvas.addEventListener("mouseleave",stopPainting)
  }
