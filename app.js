e2fa75c5e1429050a7e0903c8fcb28ed3b74ac0a
DESKTOP-S8ABVPR\82109 <hs183000@naver.com>
2022-06-12 15:52:53 +0900

    Changing Color

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
  const canvas = document.getElementById("jsCanvas");
  const ctx= canvas.getContext("2d")
+ const colors= document.getElementsByClassName("jsColor")
   
  canvas.width=700;
  canvas.height=700;
@@ -26,10 +27,12 @@
     }
     
  }
- function onMousedown(event){
-    
-    painting=true;
- }
+
+function handeleColorClick(event){
+  const color=event.target.style.backgroundColor;
+  ctx.strokeStyle=color;
+}
+
 
  
  
@@ -39,3 +42,4 @@
     canvas.addEventListener("mouseup",stopPainting)
     canvas.addEventListener("mouseleave",stopPainting)
  }
+Array.from(colors).forEach(color=>color.addEventListener("click",handeleColorClick))
