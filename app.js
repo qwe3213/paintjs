cda49452a490b99c4b54679c58d9645391825359
DESKTOP-S8ABVPR\82109 <hs183000@naver.com>
2022-06-12 19:15:51 +0900

    Brush Size

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
  const canvas = document.getElementById("jsCanvas");
  const ctx= canvas.getContext("2d")
  const colors= document.getElementsByClassName("jsColor")
-  
+ const range= document.getElementById("jsRange")
+ const mode= document.getElementById("jsMode")
  canvas.width=700;
  canvas.height=700;
 
   ctx.strokeStyle ="#2c2c2c";
   ctx.lineWidth=2.5;
  let painting =false;
+ let filling =false;
 
   function stopPainting(){
    painting=false;
@@ -32,7 +34,19 @@ function handeleColorClick(event){
   const color=event.target.style.backgroundColor;
   ctx.strokeStyle=color;
 }
-
+function handleRangeChange(event){
+   const size = event.target.value
+   ctx.lineWidth=size;
+}
+function handleModeClick(){
+   if(filling===true){
+      filling=false;
+      mode.innerText="Fill"
+   } else{
+      filling=true;
+      mode.innerText="Paint"
+   }
+}
 
  
  
@@ -43,3 +57,11 @@ function handeleColorClick(event){
     canvas.addEventListener("mouseleave",stopPainting)
  }
 Array.from(colors).forEach(color=>color.addEventListener("click",handeleColorClick))
+
+if(range){
+   range.addEventListener("input",handleRangeChange)
+}
+
+if(mode){
+   mode.addEventListener("click",handleModeClick)
+}
